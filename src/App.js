2383ad90d278a80d395b498c8a697826bb83de0a
EugeneKruglei <eugene787@yandex.ru>
2019-01-18 11:21:33 +0300

    Active buttons on click

--- src/App.js
+++ src/App.js
@@ -4,95 +4,113 @@ import {Impress, Step} from 'react-impressjs';
 import 'react-impressjs/styles/react-impressjs.css';
 import Nav from './Nav/Nav';
 
-let w = window.screen.availWidth;
-let h = window.screen.availHeight;
+let w = window.innerWidth;
+let h = window.innerHeight;
+let x = window.innerWidth * 2;
+let y = window.innerHeight * 2;
+
+/*const menuItems = [
+    'step-1',
+    'step-2',
+    'step-3',
+    'step-4',
+    'step-5'
+];*/
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            current: "active",
+            duration: 1500
+        };
+        // this.handleClick = this.handleClick.bind(this);
+    };
+/*
+    handleClick(menuItem) {
+        this.setState({active: menuItem});
+    }*/
 
+    // componentWillReceiveProps(nextProps, nextState) {
+    // this.setState({ active: menuItems[menuItems.indexOf(window.location.hash.slice(2))]});
+    // }
+
+    // menuItems[menuItems.indexOf(window.location.hash.slice(2))]
     render() {
+        console.log(window.location.hash.slice(2));
         return (
             <div>
-                <Nav/>
+                <Nav current={"active"}/>
                 <Impress
                     progress={true}
                     fallbackMessage={<p>Sorry, your <b>device or browser</b> couldn't support well.</p>}
                 >
-                    <Step exact path='/step-1' className={'home'}
+                    <Step exact path='menuItems[0]' className={window.location.hash.slice(2)==='step-1' ? "active home" : {}}
                           data={
                               {
-                                  x: 10000,
-                                  y: 10000,
+                                  x: x,
+                                  y: y,
                                   z: 10000,
-                                  rotateX: 45,
+                                  rotateX: 0,
                                   rotateY: 0,
                                   rotateZ: 0,
                                   scale: 2
-                              }} duration={1500}>
+                              }} duration={750}>
                         <p></p>
+
                     </Step>
-                    <Step path='/step-2' className={'about'} data={
+                    <Step path='menuItems[1]' className={'about'} data={
                         {
-                            x: 10000 - w/2,
-                            y: 10000,
+                            x: x - w / 2,
+                            y: y - h / 2,
                             z: 10000,
                             rotateX: 0,
-                            rotateY: 90,
+                            rotateY: 0,
                             rotateZ: 0,
                             scale: 1
-                        }} duration={1500}>
+                        }} duration={750}>
                         <p>Made by your <b>Creativity</b> !!</p>
                     </Step>
                     <Step path='menuItems[2]' className={'product'}
                           data={
                               {
-                                  x: 10000,
-                                  y: 10000,
-                                  z: 10000-w/2,
+                                  x: x + w / 2,
+                                  y: y + h / 2,
+                                  z: 10000,
                                   rotateX: 0,
                                   rotateY: 0,
                                   rotateZ: 0,
                                   scale: 1
                               }}
-                          duration={1500}>
+                          duration={750}>
                         <p>Made by your <b>Creativity</b> !!</p>
                     </Step>
                     <Step path='menuItems[3]' className={'contact'}
                           data={
                               {
-                                  x: 10000+w/2,
-                                  y: 10000,
+                                  x: x - w / 2,
+                                  y: y + h / 2,
                                   z: 10000,
                                   rotateX: 0,
-                                  rotateY: -90,
-                                  rotateZ: 0,
-                                  scale: 1
-                              }}
-                          duration={1500}>
-                        <p>Made by your <b>Creativity</b> !!</p>
-                    </Step>
-                    <Step exact path='/step-5' className={'about1'}
-                          data={
-                              {
-                                  x: 10000,
-                                  y: 10000+h/2,
-                                  z: 10000-h/2,
-                                  rotateX: 90,
                                   rotateY: 0,
                                   rotateZ: 0,
                                   scale: 1
-                              }} duration={1500}>
+                              }}
+                          duration={750}>
                         <p>Made by your <b>Creativity</b> !!</p>
                     </Step>
-                    <Step path='/step-6' className={'product1'} data={
-                        {
-                            x: 10000,
-                            y: 10000+h/2,
-                            z: 10000+w/2-h/2,
-                            rotateX: 90,
-                            rotateY: 0,
-                            rotateZ: 0,
-                            scale: 1
-                        }} duration={1500}>
+                    <Step path='menuItems[4]' className={'about1'}
+                          data={
+                              {
+                                  x: x + w / 2,
+                                  y: y - h / 2,
+                                  z: 10000,
+                                  rotateX: 0,
+                                  rotateY: 0,
+                                  rotateZ: 0,
+                                  scale: 1
+                              }} duration={750}>
                         <p>Made by your <b>Creativity</b> !!</p>
                     </Step>
                 </Impress>
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,9 +6,7 @@ const menuItems = [
     'step-2',
     'step-3',
     'step-4',
-    'step-5',
-    'step-6',
-    'step-7'
+    'step-5'
 ];
 
 
@@ -18,41 +16,60 @@ class Nav extends Component {
         this.state = {
             active: menuItems[menuItems.indexOf(window.location.hash.slice(2))],
         };
+        this.handleClick = this.handleClick.bind(this);
     };
 
-    _handleClick(menuItem) {
+    handleClick(menuItem) {
         this.setState({active: menuItem});
     }
+    // _handleKeyPress = (e) => {
+    //     if (e.key === 'Enter') {
+    //         handleKeyDown(menuItem){
+    //             this.setState({active: menuItem});
+    //         }
+    //     }
+    // }
+
+    // componentWillReceiveProps({props}) {
+    //     this.setState({...this.state, props})
+    // }
+    /*componentWillUpadate(props){
+        this.setState({...this.state, props})
+    }*/
 
     render() {
-        console.log(window.location.hash.slice(1));
+        console.log(this.props.state);
+        console.log();
         return (
             <div className="navigation">
-                <a href="#/step-1" onClick={this._handleClick.bind(this, menuItems[0])}
-                   className={this.state.active === menuItems[0] ? "active" : {}}
+                <a href="#/step-1"
+                   onClick={this.handleClick.bind(this, menuItems[0])}
+                   className={this.state.active === 'step-1' ? "active" : {}}
                 >home
                 </a>
-                <a href="#/step-2" onClick={this._handleClick.bind(this, menuItems[1])}
-                   className={this.state.active === menuItems[1] ? "active" : {}}>
+                <a href="#/step-2"
+                   ref="step-2"
+                   onClick={this.handleClick.bind(this, menuItems[1])}
+                   className={this.state.active === 'step-2' ? "active" : {}}
+                >
                     about
                 </a>
 
-                <a href="#/step-3" onClick={this._handleClick.bind(this, menuItems[2])}
-                   className={this.state.active === menuItems[2] ? "active" : {}}>
+                <a href="#/step-3"
+                   onClick={this.handleClick.bind(this, menuItems[2])}
+                   className={this.state.active === 'step-3' ? "active" : {}}>
                     product
                 </a>
 
-                <a href="#/step-4" onClick={this._handleClick.bind(this, menuItems[3])}
-                   className={this.state.active === menuItems[3] ? "active" : {}}>contact
+                <a href="#/step-4"
+                   onClick={this.handleClick.bind(this, menuItems[3])}
+                   className={this.state.active === 'step-4' ? "active" : {}}>
+                    contact
                 </a>
-                <a href="#/step-5" onClick={this._handleClick.bind(this, menuItems[4])}
-                   className={this.state.active === menuItems[4] ? "active" : {}}>
+                <a href="#/step-5"
+                   onClick={this.handleClick.bind(this, menuItems[4])}
+                   className={this.state.active === 'step-5' ? "active" : {}}>
                     about1
-                </a>
-
-                <a href="#/step-6" onClick={this._handleClick.bind(this, menuItems[5])}
-                   className={this.state.active === menuItems[5] ? "active" : {}}>
-                    product1
                 </a>
             </div>
         )
